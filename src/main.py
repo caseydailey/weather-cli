b24c4290200fab2ff09353a859b350d49208d66f
Casey Dailey <casey@feasttogether.org>
2017-11-20 12:13:57 -0600

    files, initial setup and menu printing

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+import api
+import zips
+import controller
+import views
+
+def main():
+
+    os.system('clear')
+    print(views.main_menu())
+    choice = input(">> ")
+
+if __name__ == '__main__':
+    main()
---
+++ src/views.py
@@ -0,0 +1,14 @@
+
+
+def main_menu():
+    return """
+            *****************************
+                Welcome to Weather-CLI
+            *****************************
+
+            1. Get Weather
+            2. Help
+            3. Quit
+
+            """
+
